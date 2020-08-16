ec9366b563ba1902bf97162a26d8204f2f3cef35
Abdul Momin <abdulmomin114477@gmail.com>
2020-08-16 22:50:54 +0600

    completed the likes model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,31 @@
+import { construct } from "core-js/fn/reflect";
+
+export default class Likes {
+  constructor() {
+    this.likes = [];
+  }
+
+  addLike(id, title, author, img) {
+    const like = {
+      id,
+      title,
+      author,
+      img,
+    };
+    this.likes.push(like);
+    return like;
+  }
+
+  deleteLIke(id) {
+    const index = this.likes.findIndex((el) => el.id === id);
+    this.likes.splice(index, 1);
+  }
+
+  isLiked(id) {
+    return this.likes.findIndex((el) => el.id === id) !== -1;
+  }
+
+  getNumLikes() {
+    return this.likes.length;
+  }
+}
